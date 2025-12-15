1e8aa70c9a98bbb6e6d175c876ce9499cc0b4742
sopyan <sopyan@mail.com>
2025-12-15 09:52:52 +0700

    update limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Limit;
+use Illuminate\Cache\RateLimiting\Limit;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \Illuminate\Support\Facades\RateLimiter::for('submission', function (\Illuminate\Http\Request $request) {
-            return \Illuminate\Support\Facades\Limit::perMinute(3)->by($request->ip());
+            return Limit::perMinute(3)->by($request->ip());
         });
     }
 }
